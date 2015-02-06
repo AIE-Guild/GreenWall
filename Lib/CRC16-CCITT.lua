70d7fc7386b9338a28a9cae8ea30b2d26fc53329
Mark Rogaski <mrogaski@pobox.com>
2015-02-05 19:56:57 -0500

    Removed excessive white space and added assert for input validation.

--- Lib/CRC16-CCITT.lua
+++ Lib/CRC16-CCITT.lua
@@ -46,35 +46,23 @@ local bxor, band, rshift = bit.bxor, bit.band, bit.rshift
 -- Functions
 ----------------------------------------------------------------------------
 
---- CRC16-CCITT
+--- CRC16-CCITT hash function
 -- @param str The string to hash.
 -- @return The CRC hash.
-function CRC.Hash(str)
-
-    if str == nil then
-        str = ''
-    end
-    
+function CRC.Hash(s)
+    assert(type(s) == 'string')
     local crc = 0xffff
-    
-    for i = 1, #str do
-    
-        c = str:byte(i)
+    for i = 1, #s do    
+        local c = s:byte(i)
         crc = bxor(crc, c)
-
         for j = 1, 8 do
-
             local k = band(crc, 1)
             crc = rshift(crc, 1)
-
             if k ~= 0 then
                 crc = bxor(crc, 0x8408)
             end
-
         end
-
     end
-    
     return crc
 end
 
