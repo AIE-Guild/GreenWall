a21b5c47b2a92dc085eaae90dd2bdb159deb80b8
Mark Rogaski <mrogaski@pobox.com>
2018-01-02 22:23:35 -0500

    Added unit tests. (#114)

--- tests/TestConfig.lua
+++ tests/TestConfig.lua
@@ -75,8 +75,7 @@ function TestConfigBuilder:test_get_config()
     lu.assertTrue(config:isa(GwConfig))
 end
 
-
-function TestConfigBuilder:test_version()
+function TestConfigBuilder:test_set_version()
     local builder = GwConfigBuilder:new()
     builder:set_version(1)
     lu.assertEquals(builder:get_config().version, 1)
@@ -84,6 +83,87 @@ function TestConfigBuilder:test_version()
     lu.assertEquals(builder:get_config().version, 2)
     lu.assertError(builder.set_version, builder, 3)
 end
+
+function TestConfigBuilder:test_set_guild_name()
+    local builder = GwConfigBuilder:new()
+    builder:set_guild_name('Blue Sun')
+    lu.assertEquals(builder:get_config().guild_name, 'Blue Sun')
+    lu.assertError(builder.set_guild_name, '')
+    lu.assertError(builder.set_guild_name, nil)
+end
+
+function TestConfigBuilder:test_set_guild_id()
+    local builder = GwConfigBuilder:new()
+    builder:set_guild_id('bsun')
+    lu.assertEquals(builder:get_config().guild_id, 'bsun')
+    lu.assertError(builder.set_guild_id, '')
+    lu.assertError(builder.set_guild_id, nil)
+end
+
+function TestConfigBuilder:test_set_guild_channel()
+    local builder = GwConfigBuilder:new()
+
+    builder:set_guild_channel('someChannel')
+    lu.assertEquals(builder:get_config().channel.guild,
+        {name = 'someChannel'})
+
+    builder:set_guild_channel('someChannel', 'p@ssw0rd')
+    lu.assertEquals(builder:get_config().channel.guild,
+        {name = 'someChannel', password = 'p@ssw0rd'})
+
+    builder:set_guild_channel('someChannel', 'p@ssw0rd', 'sekrit')
+    lu.assertEquals(builder:get_config().channel.guild,
+        { name = 'someChannel', password = 'p@ssw0rd', key = 'sekrit'})
+
+    builder:set_guild_channel('someChannel', 'p@ssw0rd', 'sekrit', 1)
+    lu.assertEquals(builder:get_config().channel.guild,
+        { name = 'someChannel', password = 'p@ssw0rd', key = 'sekrit', cipher = 1})
+
+    lu.assertError(builder.set_guild_channel, '')
+    lu.assertError(builder.set_guild_channel, nil)
+end
+
+function TestConfigBuilder:test_set_officer_channel()
+    local builder = GwConfigBuilder:new()
+
+    builder:set_officer_channel('someChannel')
+    lu.assertEquals(builder:get_config().channel.officer,
+        {name = 'someChannel'})
+
+    builder:set_officer_channel('someChannel', 'p@ssw0rd')
+    lu.assertEquals(builder:get_config().channel.officer,
+        {name = 'someChannel', password = 'p@ssw0rd'})
+
+    builder:set_officer_channel('someChannel', 'p@ssw0rd', 'sekrit')
+    lu.assertEquals(builder:get_config().channel.officer,
+        { name = 'someChannel', password = 'p@ssw0rd', key = 'sekrit'})
+
+    builder:set_officer_channel('someChannel', 'p@ssw0rd', 'sekrit', 1)
+    lu.assertEquals(builder:get_config().channel.officer,
+        { name = 'someChannel', password = 'p@ssw0rd', key = 'sekrit', cipher = 1})
+
+    lu.assertError(builder.set_officer_channel, '')
+    lu.assertError(builder.set_officer_channel, nil)
+end
+
+function TestConfigBuilder:test_set_peer()
+    local builder = GwConfigBuilder:new()
+    builder:set_peer('coga', 'Guild A')
+    builder:set_peer('cogb', 'Guild B')
+    builder:set_peer('cogc', 'Guild C')
+    lu.assertEquals(builder:get_config().peer, {coga="Guild A", cogb="Guild B", cogc="Guild C"})
+    lu.assertError(builder.set_peer, '')
+    lu.assertError(builder.set_peer, nil)
+end
+
+function TestConfigBuilder:test_set_minimum_version()
+    local builder = GwConfigBuilder:new()
+    builder:set_minimum_version('1.9.0-beta')
+    lu.assertEquals(builder:get_config().minimum_version, {major=1, meta={}, minor=9, patch=0, pre={"beta"}})
+    lu.assertError(builder.set_minimum_version, '')
+    lu.assertError(builder.set_minimum_version, nil)
+end
+
 
 --
 -- Run the tests
