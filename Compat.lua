1c888d954f2fa7939819d1e2529c4ea30043a7c2
Mark Rogaski <mrogaski@pobox.com>
2019-01-06 01:40:47 -0500

    Added ID tagging support for WoW 8.1.0. (#152)

--- Compat.lua
+++ Compat.lua
@@ -39,17 +39,35 @@ gw.ChatFrame_MessageEventHandler = ChatFrame_MessageEventHandler
 -- Apply workarounds
 --
 function gw.EnableCompatibility()
+    if IsAddOnLoaded('Identity-2') then
+        -- Use Identity-2 for adding a chat identifier.
+        gw.compatibility.identity = true
+        gw.Debug(GW_LOG_NOTICE, 'Identity-2 compatibility enabled.')
+    end
+    if IsAddOnLoaded('Name2Chat') then
+        -- Use Name2Chat for adding a chat identifier.
+        gw.compatibility.name2chat = true
+        gw.Debug(GW_LOG_NOTICE, 'Name2Chat compatibility enabled.')
+    end
     if IsAddOnLoaded('ElvUI') then
         -- Use ElvUI's event handler for sending messages to the chat windows
-        local status, enabled = pcall(function () return ElvUI[1].private.chat.enable end)
+        local status, enabled = pcall(function()
+            return ElvUI[1].private.chat.enable
+        end)
         if status and enabled then
+            gw.compatibility.elvui = true
             local ElvUIChat = ElvUI[1]:GetModule('Chat')
-            gw.ChatFrame_MessageEventHandler = function(...) ElvUIChat:FloatingChatFrame_OnEvent(...) end
+            gw.ChatFrame_MessageEventHandler = function(...)
+                ElvUIChat:FloatingChatFrame_OnEvent(...)
+            end
             gw.Debug(GW_LOG_NOTICE, 'ElvUI compatibility enabled.')
         end
     elseif IsAddOnLoaded('Prat-3.0') then
         -- Use Prat's event handler for sending messages to the chat windows
-        gw.ChatFrame_MessageEventHandler = function (...) Prat.Addon.ChatFrame_MessageEventHandler(Prat.Addon, ...) end
+        gw.compatibility.prat = true
+        gw.ChatFrame_MessageEventHandler = function(...)
+            Prat.Addon.ChatFrame_MessageEventHandler(Prat.Addon, ...)
+        end
         gw.Debug(GW_LOG_NOTICE, 'Prat-3.0 compatibility enabled.')
     end
 end
