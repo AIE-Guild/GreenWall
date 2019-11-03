e5e1693f4c06f5030caa6ad4e5e6f3bf9555b344
Mark Rogaski <mrogaski@pobox.com>
2019-11-02 23:09:12 -0500

    Added unit tests. (#172)

--- tests/TestUtility.lua
+++ tests/TestUtility.lua
@@ -41,9 +41,24 @@ require('Loader')
 -- Test Cases
 --
 
-TestItemInfo = {}
+TestUtility = {}
 
-function TestItemInfo:test_GetItemString()
+function TestUtility:test_iCmp()
+    lu.assertTrue(gw.iCmp("Foo", "FOO"))
+    lu.assertTrue(gw.iCmp("Foo", "Foo"))
+    lu.assertFalse(gw.iCmp("Foo", "FOOBAR"))
+end
+
+function TestUtility:test_GlobalName()
+    lu.assertEquals(gw.GlobalName("Ralff", "EarthenRing"), "Ralff-EarthenRing")
+    lu.assertEquals(gw.GlobalName("Ralff", "Earthen Ring"), "Ralff-EarthenRing")
+    lu.assertEquals(gw.GlobalName("Ralff-EarthenRing"), "Ralff-EarthenRing")
+    lu.assertEquals(gw.GlobalName("Ralff-EarthenRing", "EarthenRing"), "Ralff-EarthenRing")
+    lu.assertEquals(gw.GlobalName("Ralff"), "Ralff-EarthenRing")
+    lu.assertNil(gw.GlobalName(nil))
+end
+
+function TestUtility:test_GetItemString()
     local single = 'Testing |Hitem:6948::::::::80::::|h in a string.'
     local multi = 'Testing |Hitem:6948::::::::80::::|h and |Hitem:4388:0:0:0:0:0:0:210677200:80:0:0:0:0|h in a string.'
     lu.assertEquals(gw.GetItemString(single), 'item:6948::::::::80::::')
